b8a9ddcc93867fb01b0c2791f1759c899d871508
Rakesh Kumar <justdecodeme@hotmail.com>
2020-08-24 12:40:19 +0530

    Logged in functionality added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+/* ******************* */
+/* SIGNUP LOGIC */
+/* ******************* */
 let signupForm = document.getElementById("signupForm");
-let logOut = document.getElementById("logOut");
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,12 +10,32 @@ signupForm.addEventListener("submit", (e) => {
   const password = signupForm["signup_password"].value;
 
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
     console.log("User Signed up!");
   });
 });
 
+/* ******************* */
+/* LOGIN LOGIC */
+/* ******************* */
+let loginForm = document.getElementById("loginForm");
+let logOut = document.getElementById("logOut");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = loginForm["login_email"].value;
+  const password = loginForm["login_password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    console.log("User Logged in!");
+  });
+});
+
 logOut.addEventListener("click", () => {
-  auth.signOut().then(() => {
+  auth.signOut().then((cred) => {
+    console.log(cred.user);
     console.log("Successfully log out!");
   });
 });
