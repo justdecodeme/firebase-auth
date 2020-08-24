c1dc5e5c8b2f232eba2d0073322c0529fc267990
Rakesh Kumar <justdecodeme@hotmail.com>
2020-08-24 13:10:11 +0530

    error messages added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 /* SIGNUP LOGIC */
 /* ******************* */
 let signupForm = document.getElementById("signupForm");
+let signup_error = document.getElementById("signup_error");
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,16 +10,26 @@ signupForm.addEventListener("submit", (e) => {
   const email = signupForm["signup_email"].value;
   const password = signupForm["signup_password"].value;
 
-  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    // console.log(cred.user);
-    // console.log("User Signed up!");
-  });
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      // console.log(cred.user);
+      // console.log("User Signed up!");
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.message) {
+        signup_error.innerHTML = err.message;
+        signup_error.style.display = "block";
+      }
+    });
 });
 
 /* ******************* */
 /* LOGIN LOGIC */
 /* ******************* */
 let loginForm = document.getElementById("loginForm");
+let login_error = document.getElementById("login_error");
 let logOut = document.getElementById("logOut");
 
 loginForm.addEventListener("submit", (e) => {
@@ -27,10 +38,19 @@ loginForm.addEventListener("submit", (e) => {
   const email = loginForm["login_email"].value;
   const password = loginForm["login_password"].value;
 
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // console.log(cred.user);
-    // console.log("User Logged in!");
-  });
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((cred) => {
+      // console.log(cred.user);
+      // console.log("User Logged in!");
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.message) {
+        login_error.innerHTML = err.message;
+        login_error.style.display = "block";
+      }
+    });
 });
 
 logOut.addEventListener("click", () => {
